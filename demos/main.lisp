4a74939a2a83102740dc79c65f8a4488e43be40c
Mikhail Michurov <mikhail.michurov.00@gmail.com>
2024-07-12 03:49:55 +0700

    Replace division by zero by print

--- demos/main.lisp
+++ demos/main.lisp
@@ -1,4 +1,4 @@
 (import "demos/lists.lisp")
 (import "demos/range.lisp")
 
-(apply (fn (x) (/ 1 0)) (chunk-by 13 (map (fn (x) (list x (* x x))) (range 100))))
+(apply print (chunk-by 13 (map (fn (x) (list x (* x x))) (range 100))))
