566fd4415d6f1b5e3c7a131796c3eb0486e287c3
Mikhail Michurov <mikhail.michurov.00@gmail.com>
2024-07-14 19:24:26 +0700

    Experiment with structured parameter binding

--- demos/main.scm
+++ demos/main.scm
@@ -1,4 +1,8 @@
 (import "demos/lists.scm")
 (import "demos/range.scm")
 
-(apply print (chunk-by 6 (map (fn (x) (list x (* x x))) (range 500))))
+(defn main ()
+  (let ((squares (map (fn (x) (list x (* x x))) (range 500))))
+    (apply print (chunk-by 3 (map (fn ((num squared)) (list num 'squared 'is squared)) squares)))))
+
+(main)
