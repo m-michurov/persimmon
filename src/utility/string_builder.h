800dd7342a633b4884e782dee3660dc1e3ab3ed4
Mikhail Michurov <mikhail.michurov.00@gmail.com>
2024-07-28 03:47:21 +0700

    Mark string builder functions nodiscard

--- src/utility/string_builder.h
+++ src/utility/string_builder.h
@@ -15,8 +15,11 @@ void sb_free(StringBuilder *sb);
 
 void sb_clear(StringBuilder *sb);
 
+[[nodiscard]]
 bool sb_try_reserve(StringBuilder *sb, size_t max_length, errno_t *error_code);
 
+[[nodiscard]]
 bool sb_try_printf(StringBuilder *sb, char const *format, ...);
 
+[[nodiscard]]
 bool sb_try_printf_realloc(StringBuilder *sb, errno_t *error_code, char const *format, ...);
